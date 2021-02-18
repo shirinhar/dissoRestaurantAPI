713f5e836f715199545a081da17fe56537eeee7f
shirinhar <shirin.harandi.17@ucl.ac.uk>
2021-02-18 16:09:31 +0000

    refactorrrr

--- application.py
+++ application.py
@@ -1,12 +1,6 @@
-from flask import request
 from flask import Flask
-import requests
-import json
-
 app = Flask(__name__)
 
-@app.route("/hi")
-def api_root():
-    return 'Welcome to the chatbot from hell'
-
-
+@app.route("/")
+def hello():
+    return "Hello, World!"
