3490f70f80be07a618a581bbe44ded4cf1d92a13
Shirin Harandi <32335494+shirinhar@users.noreply.github.com>
2021-02-18 13:12:18 +0000

    Add files via upload

---
+++ dialogflow-server.py
@@ -0,0 +1,78 @@
+from flask import request
+from flask import Flask
+import requests
+import json
+
+full_results = []
+main_db = {}
+
+def get_db():
+
+    x = requests.get('https://40435.wayscript.io/main')
+    print(x.json())
+
+    return x.json()
+
+def params_dict(tracker):
+    search_params = dict()
+
+    params = ["area", "name", "pricerange", "food"]
+
+    for param in params:
+        try:
+            slot = tracker[param]
+            if slot != None:
+                search_params[param] = slot
+        except:
+            pass
+
+    return search_params
+
+def get_corresponding_value(params):
+
+    res = []
+
+
+    for k, val in main_db.items():
+        full_match = True
+
+        for p_k, p_val in params.items():
+
+            if not val[p_k] == p_val:
+                full_match = False
+
+        if full_match:
+            res.append(val)
+    # print(res)
+    return res
+
+def action_suggest(tracker):
+
+    search_params = params_dict(tracker)
+    full_results = get_corresponding_value(search_params)
+
+    if bool(full_results):
+        # ret = {}
+        for r in full_results:
+
+            return json.dumps({"name": r['name'], "phone": r['phone'], "address": r['addr']})
+        # return ret
+
+
+    return {}
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'Welcome to the chatbot from hell'
+
+@app.route('/get_reccomendations', methods=['POST'])
+def api_gh_message():
+    # print()
+    return action_suggest(request.json)
+
+if __name__ == '__main__':
+    main_db = get_db()
+    app.run(debug=True)
